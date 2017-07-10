7144690cdb351e23dd84bf4c0982e7e76a6c3f5c
vorobjevdn <vorobjevdn@mail.ru>
2017-07-10 10:17:48 +0300

    PAVR v1.2
    PAVR v1.2

---
+++ PAVR v1.2/content.js
@@ -0,0 +1,6 @@
+var element = document.getElementById("age-verification-container");
+element.outerHTML = "";
+delete element;
+element = document.getElementById("age-verification-wrapper");
+element.outerHTML = "";
+delete element;
